a3904c0e07d2ddbda798ebd27a27567116228a5a
Exterras <choi3377@naver.com>
2016-04-25 08:55:19 +0900

    20160425 08:55:00 update

--- src/com/github/Exterras/calculator/Calculator.java
+++ src/com/github/Exterras/calculator/Calculator.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Calculator {
 	
 	private boolean isExit = true;
-	private int inputOperator;
-	
+//	private int inputOperator; 
+
 	private void printAdd(int a, int b) {
 		System.out.println(a + b);
 	}
@@ -18,59 +18,66 @@ public class Calculator {
 	private void printMulti(int a, int b) {
 		System.out.println(a * b);
 	}
+	 
 	
 	public boolean isExit() {
 		return isExit;
-	}
-	
-	private void setExit(boolean isExit) {
-		this.isExit = isExit;
-	}
-	
-	private void inputOperMassage(){
+	} 
+
+	private boolean inputOperMassage(int inputOperator){
 		if (inputOperator == 1) {
 			System.out.println("덧셈을 할 두 수를 입력하세요.");
 		} else if (inputOperator == 2) {
 			System.out.println("뺄셈을 할 두 수를 입력하세요.");
 		} else if (inputOperator == 3) {
 			System.out.println("곱셈을 할 두 수를 입력하세요.");
-		} else {
-			System.out.println("잘못 입력하셨습니다.");
-		}
-	}
-	
-	public boolean play() {
-		Scanner s = new Scanner(System.in);
-
-		System.out.println("계산기입니다. 원하시는 연산을 선택하세요.");
-		System.out.println("1. 덧셈" + "\t" + "2. 뺄셈" + "\t" + "3. 곱셈" + "\t" + "4. 종료");
-
-		inputOperator = s.nextInt();
-		
-		if (inputOperator == 4) {
+		} else if(inputOperator == 4){
 			System.out.println("계산기를 종료합니다.");
-			setExit(false); 
+//			isExit = false;
 			/* field isExit = false; -> CalculatorDo while else -> break;
 			-> CalculatorDo All Completed -> terminated */
 			return false;
 			// public boolean play() return false -> terminated.
-		}
-		
-		inputOperMassage();
-		
-		int inputNum1 = s.nextInt();
-		int inputNum2 = s.nextInt();
-		
-		if (inputOperator == 1) {
-			printAdd(inputNum1, inputNum2);
-		} else if (inputOperator == 2) {
-			printMinus(inputNum1, inputNum2);
-		} else if (inputOperator == 3) {
-			printMulti(inputNum1, inputNum2);
-		} else {
+		}else{
 			System.out.println("잘못 입력하셨습니다.");
 		}
-		return false;
+		
+		return true;
+	}
+	
+	public void play() {
+		Scanner s = new Scanner(System.in);
+		int inputOperator;
+		System.out.println("계산기입니다. 원하시는 연산을 선택하세요.");
+		System.out.println("1. 덧셈" + "\t" + "2. 뺄셈" + "\t" + "3. 곱셈" + "\t" + "4. 종료");
+
+		inputOperator = s.nextInt(); 
+		
+//		if (inputOperator == 4) {
+//			System.out.println("계산기를 종료합니다.");
+//			isExit = false;
+//			/* field isExit = false; -> CalculatorDo while else -> break;
+//			-> CalculatorDo All Completed -> terminated */
+//			return false;
+//			// public boolean play() return false -> terminated.
+//		}
+		
+		isExit = inputOperMassage(inputOperator);
+		
+		if(isExit){
+			int inputNum1 = s.nextInt();
+			int inputNum2 = s.nextInt();
+			
+			if (inputOperator == 1) {
+				printAdd(inputNum1, inputNum2);
+			} else if (inputOperator == 2) {
+				printMinus(inputNum1, inputNum2);
+			} else if (inputOperator == 3) {
+				printMulti(inputNum1, inputNum2);
+			} else {
+				System.out.println("잘못 입력하셨습니다.");
+			}
+		} 
 	}
 	// All Method Completed. -> Calculator terminated. 
 }
--- src/com/github/Exterras/calculator/CalculatorDo.java
+++ src/com/github/Exterras/calculator/CalculatorDo.java
@@ -3,12 +3,16 @@ package com.github.Exterras.calculator;
 public class CalculatorDo {
 	public static void main(String[] args) {
 		Calculator cal = new Calculator();
-		while(true){
-			if(cal.isExit()){
-				cal.play();
-			} else {
-				break;	
-			}
+		
+//		while(true){
+//			if(cal.isExit()){
+//				cal.play();
+//			} else {
+//				break;	
+//			}
+//		}
+		while(cal.isExit()){
+			cal.play();
 		}
 	} 
 }
